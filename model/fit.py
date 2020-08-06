264b2518e29ce9af47d5f9211f9be6c0bb4c69d2
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-06 17:00:31 +0530

    mean and std calculation

--- model/fit.py
+++ model/fit.py
@@ -3,7 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
+import torchvision.transforms as transforms
 from torchvision.datasets import MNIST
 
 data = MNIST(root="./data", download=True)
+# about the data
 print(data)
+
+data = MNIST(root="./data", download=False, transform=transforms.ToTensor())
+i, j = data[0]
+mean = []
+std = []
+for x in range(len(i)):
+    mean.append(i[x].mean())
+    std.append(i[x].std())
+# mean and std for normalization
+print(mean)
+print(std)
