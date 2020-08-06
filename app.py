39f51e2cf218bfec2c053c095c96ae53371ef426
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-06 19:19:26 +0530

    fully functional server with ugly ui

--- app.py
+++ app.py
@@ -18,5 +18,18 @@ def test():
     return render_template("index.htm")
 
 
+@app.route("/predict2", methods=["GET", "POST"])
+def test2():
+    if request.method == "POST":
+        file = request.files.get("test")
+        file = file.read()
+        x = transformer(file)
+        x = predicts(x)
+        print(x.item())
+        return render_template("index2.htm",
+                               pred=f"{x.item()} is the number in the image")
+    return render_template("index2.htm", pred="select a image for prediciton")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
