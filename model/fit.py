7d065d59db2987c0f21a46344c5bab90536fc507
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-06 17:57:47 +0530

    done training

--- model/fit.py
+++ model/fit.py
@@ -35,7 +35,10 @@ transform = transforms.Compose(
 
 data = MNIST(root="./data", download=False, transform=transform, train=True)
 
-dl = DataLoader(data, batch_size=64, shuffle=True)
+train, vali = torch.utils.data.random_split(data, [50000, 10000])
+
+dl = DataLoader(train, batch_size=64, shuffle=True)
+vl = DataLoader(vali, batch_size=64, shuffle=True)
 
 model = Fnn(784, 64, 10)
 
@@ -52,3 +55,29 @@ def accuracy(pred, label):
 loss = nn.CrossEntropyLoss()
 print(loss(pred, j))
 print(accuracy(pred, j))
+
+
+def fit(epochs, lr, model, loss, dl, vl):
+    opt = optim.SGD(model.parameters(), lr=lr)
+    for epoch in range(epochs):
+        for i, j in dl:
+            pred = model(i)
+            ls = loss(pred, j)
+            ls.backward()
+            opt.step()
+
+        for i, j in vl:
+            pred = model(i)
+            ls = loss(pred, j)
+            acc = accuracy(pred, j)
+            print(f"epoch.no={epoch+1} loss={ls} acc={acc}")
+
+
+epochs = 20
+lr = 2e-5
+model = Fnn(784, 64, 10)
+loss = nn.CrossEntropyLoss()
+
+fit(epochs, lr, model, loss, dl, vl)
+
+torch.save(model.state_dict(), "fnn.pth")
